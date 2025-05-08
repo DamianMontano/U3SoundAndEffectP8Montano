3a624d05c3eceafc60851d16b8a790a5e2ab8ec1
DamianMontano <dee3montano@gmail.com>
2025-05-08 15:23:00 -0700

    Worked on my project
    Added new things to my scripts

--- P8U3SoundAndEffectMontano/Assets/Scripts/PlayerController.cs
+++ P8U3SoundAndEffectMontano/Assets/Scripts/PlayerController.cs
@@ -3,16 +3,19 @@ using System.Collections.Generic;
 using System.Linq.Expressions;
 using UnityEngine;
 
-public class PlayuerController : MonoBehaviour
+public class PlayerController : MonoBehaviour
 {
     private Rigidbody playerRb;
+    private Animator playerAnim;
     public float jumpForce = 10;
     public float gravityModifier;
     public bool isOnGround = true;
+    public bool gameOver;
     // Start is called before the first frame update
     void Start()
     {
         playerRb = GetComponent<Rigidbody>();
+        playerAnim = GetComponent<Animator>();
         Physics.gravity *= gravityModifier;
     }
 
@@ -23,10 +26,20 @@ public class PlayuerController : MonoBehaviour
         {
             playerRb.AddForce(Vector3.up * 10, ForceMode.Impulse);
             isOnGround = false;
+            playerAnim.SetTrigger("Jump_trig"); 
         }
     }
     private void OnCollisionEnter(Collision collision)
     {
-        isOnGround = true;
+        
+
+        if(collision.gameObject.CompareTag("Ground"))
+        {
+            isOnGround = true;
+        }else if(collision.gameObject.CompareTag("Obstacle"))
+        {
+            Debug.Log("Game Over");
+            gameOver = true;
+        }
     }
 }
